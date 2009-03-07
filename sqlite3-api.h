3b461eae78b180925ac6f245e60226b15ecb413b
zb <zb@3e8.org>
2009-03-07 03:40:25 -0600

    progress

---
+++ sqlite3-api.h
@@ -0,0 +1,23 @@
+int sqlite3_close(sqlite3 *);
+int sqlite3_exec(
+  sqlite3*,                                  /* An open database */
+  const char *sql,                           /* SQL to be evaluated */
+  int (*callback)(void*,int,char**,char**),  /* Callback function */
+  void *,                                    /* 1st argument to callback */
+  char **errmsg                              /* Error msg written here */
+);
+int sqlite3_open(
+  const char *filename,   /* Database filename (UTF-8) */
+  sqlite3 **ppDb          /* OUT: SQLite db handle */
+);
+int sqlite3_errcode(sqlite3 *db);
+const char *sqlite3_errmsg(sqlite3 *db);
+int sqlite3_extended_errcode(sqlite3 *db);   /* >= 3.6.5 */
+int sqlite3_prepare_v2(
+  sqlite3 *db,            /* Database handle */
+  const char *zSql,       /* SQL statement, UTF-8 encoded */
+  int nByte,              /* Maximum length of zSql in bytes. */
+  sqlite3_stmt **ppStmt,  /* OUT: Statement handle */
+  const char **pzTail     /* OUT: Pointer to unused portion of zSql */
+);
+
