17278b652a8675ed64897cade182f8361c41d0d9
zb <zb@3e8.org>
2009-03-16 19:37:58 -0500

    add more tests

--- test.scm
+++ test.scm
@@ -53,20 +53,59 @@
 (call-with-database ":memory:" (lambda (db) (with-transaction db (lambda () (call-with-prepared-statement db "select 1 union select 2" (lambda (s) (fetch (execute s)) (error 'oops))) #f))))   ; => error, same as previous
 (call-with-database ":memory:" (lambda (db) (with-transaction db (lambda () (call-with-prepared-statement db "select 1 union select 2" (lambda (s) (fetch (execute s)))) #f))))   ; => #f, statement finalized by call-with-prepared-statement
 
-(call-with-database ":memory:"
-   (lambda (db)
-     (let-prepare db ((s1 "select 1 union select 2")
-                      (s2 "select 3 union select 4"))
-        (list (fetch (execute s1)) (fetch (execute s2)))))) ;=> ((1) (3))
-
-(call-with-database ":memory:" (lambda (db) (execute-sql db "create table cache(k,v);") (execute-sql db "insert into cache values('jml', 'oak');"))) ;=> 1
-
-
-(let ((s (call-with-database ":memory:" (lambda (db) (execute-sql db "create table cache(k,v);") (let-prepare db ((s "insert into cache values('jml', 'oak');")) s))))) (execute s))  ;=> Error: operation on closed database
-
-(let ((s (call-with-database ":memory:" (lambda (db) (execute-sql db "create table cache(k,v);") (let-prepare db ((s "insert into cache values('jml', 'oak');")) s))))) (execute s))  ;=> Error: operation on closed database
-
 |#
+
+(raise-database-errors #t)
+
+(test "Pending open queries are finalized after let-prepare"
+      '((1) (3))
+      (let ((db (open-database ":memory:")))
+        (let ((rv 
+               (let-prepare db ((s1 "select 1 union select 2")
+                                (s2 "select 3 union select 4"))
+                 (list (fetch (execute s1)) (fetch (execute s2))))))
+          (close-database db)
+          rv)))
+
+;; let-prepare finalization will error when database is closed
+(test "Pending open queries are finalized when DB is closed"
+      '((1) (3))
+      (let* ((db (open-database ":memory:"))
+             (s1 (prepare db "select 1 union select 2"))
+             (s2 (prepare db "select 3 union select 4")))
+        (let ((rv (list (fetch (execute s1)) (fetch (execute s2)))))
+          (close-database db) ; finalize here
+          rv)))
+
+;; let-prepare finalization will error when database is closed
+(test "Finalizing previously finalized statement succeeds after DB is closed"
+      '((1) (3))
+      (let ((db (open-database ":memory:")))
+        (let-prepare db ((s1 "select 1 union select 2")
+                         (s2 "select 3 union select 4"))
+          (let ((rv (list (fetch (execute s1)) (fetch (execute s2)))))
+            (close-database db)
+            rv))))
+
+(test-error "Pending statements are finalized on error in call-with-database"
+            ;; Should receive error 'oops here
+            ;; FIXME should test that statements are actually finalized
+            ;; rather than relying on error 'oops reception and manual
+            ;; inspection of warning
+      (call-with-database ":memory:"
+        (lambda (db)
+          (let-prepare db ((s1 "select 1 union select 2")
+                           (s2 "select 3 union select 4"))
+            (list (fetch (execute s1))
+                  (error 'oops)
+                  (fetch (execute s2)))))))
+
+(test "create / insert one row via execute-sql"
+      1
+      (call-with-database ":memory:"
+        (lambda (db)
+          (execute-sql db "create table cache(k,v);")
+          (execute-sql db "insert into cache values('jml', 'oak');"))))
 
 (test-error   ;; operation on finalized statement
  "Execute after finalize fails"
@@ -86,18 +125,21 @@
               (prepare db "insert into cache values('jml', 'oak');")))))
    (execute s))) 
 
-(test "Double finalization in let-prepare + execute" 1
+(test "Double finalization ignored in let-prepare + execute"
+      1
       (call-with-database ":memory:"
         (lambda (db)
           (execute-sql db "create table cache(k,v);")
           (let-prepare db ((s "insert into cache values('jml', 'oak');"))
             (execute s)))))
 
-(test "Successful rollback outside transaction" #t
+(test "Successful rollback outside transaction"
+      #t
       (call-with-database ":memory:"
         (lambda (db) (rollback db))))
 
-(test "Successful commit outside transaction" #t
+(test "Successful commit outside transaction"
+      #t
       (call-with-database ":memory:"
         (lambda (db) (commit db))))
 
