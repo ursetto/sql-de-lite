b6908bb40bdb3fa67d01ceef6ee027a755f238ed
zb <zb@3e8.org>
2009-03-20 21:57:33 -0500

    lru good

--- lru.scm
+++ lru.scm
@@ -1,5 +1,17 @@
 (use srfi-69)
 
+;; FIXME: accessors must be updated to silently fail when cache capacity
+;; is 0.
+
+(declare
+ (fixnum))
+
+(cond-expand
+ (compiling)
+ (else
+  (define-syntax define-inline
+    (syntax-rules () ((_ args ...) (define args ...))))))
+
 (define-record lru-cache ht head tail capacity deleter)
 (define-record node prev next key value)
 
@@ -9,7 +21,10 @@
   (let ((ht (make-hash-table comparator)))
     (%make-lru-cache ht #f #f capacity on-delete)))
 
-(define (splice-out! x)
+(define-inline (lookup c k)
+  (hash-table-ref/default (lru-cache-ht c) k #f))
+
+(define-inline (splice-out! x)
   (let ((p (node-prev x))
         (n (node-next x)))
     (when p
@@ -21,7 +36,7 @@
     n))
 
 (define (lru-cache-ref c k)
-  (and-let* ((n (hash-table-ref/default (lru-cache-ht c) k #f)))
+  (and-let* ((n (lookup c k)))
     (if (not (node-prev n))             ; mru
         (node-value n)
         (begin
@@ -36,10 +51,13 @@
             )))))
 
 (define (lru-cache-set! c k v)
-  (let ((old (hash-table-ref/default (lru-cache-ht c) k #f)))
+  (let ((old (lookup c k)))
     (if old
         (node-value-set! old v)
         (let ((new (make-node #f (lru-cache-head c) k v)))
+          (when (>= (lru-cache-size c)
+                    (lru-cache-capacity c))  ; FIXME assert difference is 0
+            (lru-cache-delete! c (node-key (lru-cache-tail c))))
           (unless (lru-cache-tail c)
             (lru-cache-tail-set! c new))
           (when (lru-cache-head c)
@@ -47,9 +65,18 @@
           (lru-cache-head-set! c new)
           (hash-table-set! (lru-cache-ht c) k new)))))
 
+;; Missing association is not an error.
 (define (lru-cache-delete! c k)
-  (when (lru-cache-deleter c)
-    ((lru-cache-deleter c) v)))
+  (and-let* ((n (lookup c k)))
+    (hash-table-delete! (lru-cache-ht c) k)
+    (when (eq? n (lru-cache-tail c))
+      (lru-cache-tail-set! c (node-prev n)))
+    (when (eq? n (lru-cache-head c))
+      (lru-cache-head-set! c (node-next n)))
+    (splice-out! n)
+    (if (lru-cache-deleter c)
+        ((lru-cache-deleter c) k (node-value n))
+        #t)))
 
 (define (lru-cache-size c)
   (hash-table-size (lru-cache-ht c)))
@@ -70,7 +97,7 @@
       ((not n))
     (proc (node-key n) (node-value n))))
 
-
+#|
 
 
 (define C (make-lru-cache 5 string=?))
@@ -85,6 +112,19 @@
 (eq? (node-value (lru-cache-head C)) 'hi-there)  ; #t
 
 
-
+(lru-cache-set! C "a" 1)
+(lru-cache-set! C "b" 2)
+(lru-cache-set! C "c" 3)
+(lru-cache-ref C "hi there")
 (lru-cache-walk C (lambda (k v) (write (cons k v)) (newline)))
+     ;; ("hi there" . hi-there)
+     ;; ("c" . 3)
+     ;; ("b" . 2)
+     ;; ("a" . 1)
+     ;; ("bye there" . bye-there)
 
+(lru-cache-set! C "d" 4)   ; should roll off "bye there"
+(lru-cache-set! C "e" 5)   ; should roll off "a"
+(lru-cache-set! C "b" 3)   ; should change value of b to 3, does not alter order
+
+|#
