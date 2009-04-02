792351c745c4f0188752bc521c5a448ec5773913
zb <zb@3e8.org>
2009-04-02 00:31:57 -0500

    add license, meta, bump setup version

--- sql-de-lite.scm
+++ sql-de-lite.scm
@@ -1,11 +1,7 @@
-;;; simple sqlite3 interface
+;;; sql-de-lite interface to SQLite 3
 
-;; This type of query need only be stepped once, after which
-;; it will return DONE.
-;; => "INSERT INTO attached_db.temp_table SELECT * FROM attached_db.table1;"
-
-;; LRU cache lookup (and therefore update) does not occur on
-;; statement access, only on preparation.
+;; Copyright (c) 2009 Jim Ursetto.  All rights reserved.
+;; BSD license at end of file.
 
 ;;; Direct-to-C
 
@@ -832,3 +828,29 @@ int busy_notification_handler(void *ctx, int times) {
                        #t))))))))))
 
   )  ; module
+
+;; Copyright (c) 2009 Jim Ursetto.  All rights reserved.
+;; 
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are met:
+;; 
+;;  Redistributions of source code must retain the above copyright notice,
+;;   this list of conditions and the following disclaimer.
+;;  Redistributions in binary form must reproduce the above copyright notice,
+;;   this list of conditions and the following disclaimer in the documentation
+;;   and/or other materials provided with the distribution.
+;;  Neither the name of the author nor the names of its contributors 
+;;   may be used to endorse or promote products derived from this software 
+;;   without specific prior written permission.
+;; 
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+;; THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+;; PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+;; CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+;; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+;; PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;; PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;; LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
