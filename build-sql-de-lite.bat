44e0ce294ee0b682c14c7956bb0596f564868082
felix <felix@call-with-current-continuation.org>
2020-01-13 10:26:45 +0100

    Use fixup script to compile sqlite3.c separately and only pass the object file.
    The proper way would be to use c-object egg components, but that is not
    supported on older versions of CHICKEN 5, so we add this admittedly ugly
    workaround.

--- build-sql-de-lite.bat
+++ build-sql-de-lite.bat
@@ -2,5 +2,11 @@
 
 SET SQLITE3_OPTIONS=-C -DSQLITE_ENABLE_FTS3 -C -DSQLITE_ENABLE_FTS3_PARENTHESIS -C -DSQLITE_THREADSAFE=0
 
+%CHICKEN_CSI% -s fixup.scm -s %* > static.tmp
+SET /P STATIC=<static.tmp
+%CHICKEN_CSI% -s fixup.scm -o %* > opts.tmp
+SET /P OPTS=<opts.tmp
+
 REM For now we just always build with the shipped sqlite3 version.
-%CHICKEN_CSC% -C %CFLAGS% -L %LDFLAGS% %* -Isqlite3 sqlite3/sqlite3.c %SQLITE3_OPTIONS% -C -DSQLITE_THREADSAFE=0
+IF NOT EXISTS sqlite3\sqlite3%STATIC%.o %CHICKEN_CSC% %OPTS% -C %CFLAGS% -L %LDFLAGS% -c -Isqlite3 sqlite3/sqlite3.c %SQLITE3_OPTIONS% -o sqlite3/sqlite3%STATIC%.o
+%CHICKEN_CSC% -C %CFLAGS% -L %LDFLAGS% %* -Isqlite3 sqlite3/sqlite3.c %SQLITE3_OPTIONS% -C -DSQLITE_THREADSAFE=0 sqlite3/sqlite3%STATIC%.o
